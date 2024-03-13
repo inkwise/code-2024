67402e5f436a614e9b2e79d75ee2a640044dd711
inkwise <vacaaa@163.com>
2024-03-14 01:48:41 +0800

    rm target files

--- algorithm/insert_sorting/target/debug/deps/insert_sorting-ed344e5b2c467dd4.d
+++
@@ -1,5 +0,0 @@
-/home/ubuntu/code-2024/algorithm/insert_sorting/target/debug/deps/libinsert_sorting-ed344e5b2c467dd4.rmeta: src/main.rs
-
-/home/ubuntu/code-2024/algorithm/insert_sorting/target/debug/deps/insert_sorting-ed344e5b2c467dd4.d: src/main.rs
-
-src/main.rs:
--- algorithm/insert_sorting/target/debug/insert_sorting.d
+++
@@ -1,1 +0,0 @@
-/home/ubuntu/code-2024/algorithm/insert_sorting/target/debug/insert_sorting: /home/ubuntu/code-2024/algorithm/insert_sorting/src/main.rs
